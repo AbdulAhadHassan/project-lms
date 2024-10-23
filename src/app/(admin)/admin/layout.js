3cbee82be713c7c81235c71b52130d15979b15c0
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-10-24 03:05:07 +0500

    add admin pages

---
+++ src/app/(admin)/admin/layout.js
@@ -0,0 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Link from "next/link"
+
+
+export default function AdminLayout({ children }) {
+    return (
+      <div>
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList className="w-full">
+            <Link href="/admin/dashboard"><TabsTrigger value="dashboard">Dashboard</TabsTrigger></Link>
+            <Link href="/admin/trainers"><TabsTrigger value="trainers">Trainers</TabsTrigger></Link>
+            <Link href="/admin/students"><TabsTrigger value="students">Students</TabsTrigger></Link>
+            <Link href="/admin/batches"><TabsTrigger value="batches">Batches</TabsTrigger></Link>
+            <Link href="/admin/courses"><TabsTrigger value="courses">Courses</TabsTrigger></Link>
+          </TabsList>
+        </Tabs>
+        {children}
+      </div>
+    );
+  }
