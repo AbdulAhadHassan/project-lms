491686fba7dca1848f2c575789e4d0627f1be5b8
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-11-02 10:57:45 +0500

    resolved err

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,12 +1,12 @@
-import { dbConnect } from "@/lib/dbConnect";
+import { connectDB } from "@/lib/dbConnect";
 import UserModal from "@/lib/modal/UserModal"
 
 export async function POST(request){
-    await dbConnect();
+    await connectDB();
 const obj = await request.json();
 console.log("obj=>",obj);
-// const user = await UserModal.findOne({email : obj.email})
-// console.log("user=>",user);
+const user = await UserModal.findOne({email : obj.email})
+console.log("user=>",user);
 return Response.json({message: "Hello post dbmongo"});
 }
 export async function GET(request){
--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
-export const dbConnect = async () => {
+
+export async function connectDB() {
   try {
     let connection;
-    console.log("connection=>",connection?.connection?.readyState);
-    if(connection?.connection?.readyState != 1){
-      connection = await mongoose.connect(process.env.MONGO_URL);
-      console.log("Database connected successfully");
-
+    console.log("connection?.connection=>", connection?.connection);
+    if (connection?.connection?.readyState != 1) {
+      connection = await mongoose.connect(process.env.MONGODB_URL);
+      console.log("DB Connected");
     }
-
-  }catch(err){
-    console.log(err);
-
-  }
-};
+  } catch (err) {
+    console.log("err=>", err);
+  } 
+}
